7188c8be41e89fbb9467d1d62d600da2c9cbfac6
Camilo <brrodriguezd@unal.edu.co>
2022-11-11 18:11:04 -0500

    Heaps

--- arboles/MinHeap.h
+++ arboles/MinHeap.h
@@ -2,14 +2,14 @@
 #include <limits>
 #include <vector>
 
-template <class T> class MaxHeap {
+template <class T> class MinHeap {
 private:
   int maxSize;
   int size;
   std::vector<T> heap;
 
 public:
-  MaxHeap(int size) {
+  MinHeap(int size) {
     this->maxSize = size;
     this->size = 0;
     this->heap.resize(size);
@@ -19,7 +19,7 @@ public:
   int rightChild(int index) { return index * 2 + 1; }
   void SiftUp(int index) {
     while (index > 1 &&
-           (this->heap[parent(index) - 1] < this->heap[index - 1])) {
+           (this->heap[parent(index) - 1] > this->heap[index - 1])) {
       T temp = this->heap[index - 1];
       this->heap[index - 1] = this->heap[parent(index) - 1];
       this->heap[parent(index)-1] = temp;
@@ -29,11 +29,11 @@ public:
   void SiftDown(int index) {
     auto maxIndex = index;
     auto left = leftChild(index);
-    if (left <= this->size && this->heap[left-1] > this->heap[maxIndex-1]) {
+    if (left <= this->size && this->heap[left-1] < this->heap[maxIndex-1]) {
       maxIndex = left;
     }
     auto right = rightChild(index);
-    if (right <= this->size && this->heap[right-1] > this->heap[maxIndex-1]) {
+    if (right <= this->size && this->heap[right-1] < this->heap[maxIndex-1]) {
       maxIndex = right;
     }
     if (index != maxIndex) {
@@ -51,7 +51,7 @@ public:
     this->size += 1;
     SiftUp(this->size);
   }
-  T ExtractMax() {
+  T ExtractMin() {
     auto result = this->heap[0];
     this->heap[0] = this->heap[this->size - 1];
     this->size -= 1;
@@ -59,14 +59,14 @@ public:
     return result;
   }
   void Remove(int index) {
-    this->heap[index-1] = std::numeric_limits<int>::max();
+    this->heap[index-1] = std::numeric_limits<int>::min();
     SiftUp(index);
-    ExtractMax();
+    ExtractMin();
   }
   void ChagePriotiry(int index, T dato) {
     auto oldp = this->heap[index-1];
     this->heap[index-1] = dato;
-    if (dato > oldp) {
+    if (dato < oldp) {
       SiftUp(index);
     } else {
       SiftDown(index);
